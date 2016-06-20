c12c91f5b48ba057f554c998e9e152b8b7436b45
daeyoung <ndy766@naver.com>
2016-06-20 18:17:51 +0900

    lecture vo 작성중

--- UspaceAcademy/src/com/uspaceacademy/vo/Lecture.java
+++ UspaceAcademy/src/com/uspaceacademy/vo/Lecture.java
@@ -2,5 +2,9 @@ package com.uspaceacademy.vo;
 
 public class Lecture
 {
-
+	private int lectureNo;
+	private String lectureTitle;
+	private String lectureDescription;
+	private int lectureStart;
+	
 }
